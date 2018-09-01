3431155a009a4d428adc4195e257b500096ecaf0
Aleksei Pavlov <pavlov.alexei78@gmail.com>
2018-09-01 16:41:53 +0300

    Add TokenDto

---
+++ Blog.Common/Dto/TokenDto.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Common.Dto
+{
+    public class TokenDto
+    {
+        [JsonProperty("username")]
+        public string Name { get; set; }
+
+        [JsonProperty("access_token")]
+        public string Token { get; set; }
+    }
+}
