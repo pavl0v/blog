6b035839b73adfb99e1332ab0d0bf96ecb463108
Aleksei Pavlov <pavlov.alexei78@gmail.com>
2018-09-02 21:14:44 +0300

    Minor changes

--- Blog.Client/Services/ServiceBase.cs
+++ Blog.Client/Services/ServiceBase.cs
@@ -9,6 +9,7 @@ namespace Blog.Client.Services
 
         public ServiceBase(HttpClient client)
         {
+            // TODO : put settings to config file
             client.BaseAddress = new Uri("http://localhost:5000/api/");
             //client.DefaultRequestHeaders.Add("Accept", "");
             //client.DefaultRequestHeaders.Add("User-Agent", "");
